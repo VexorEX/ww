921e2e635c72ea987dcf3a2969a02810564c6104
VexorEX <Voltrakx@gmail.com>
2025-10-16 09:32:11 +0330

    Delete bulid version

--- dist/components/setCountry.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.setCountry = void 0;
-const setCountry = (bot) => {
-    const ctx = bot.ctx;
-    bot.action('setCountry_');
-};
-exports.setCountry = setCountry;
-exports.default = exports.setCountry;
